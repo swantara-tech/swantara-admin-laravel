73183bea7e56d4be30ea4cef5ca9e7a62dc37fae
presidendjakarta <presidendjakarta@gmail.com>
2026-06-18 17:39:49 +0700

    Revert "Fix: Wheel picker overlap issue and improve layout"
    This reverts commit 1bfcac1005bfc11a1367179d35347d5b854f5dba.

--- resources/views/pages/forms/datetimepicker.blade.php
+++ resources/views/pages/forms/datetimepicker.blade.php
@@ -13,16 +13,10 @@
     }
 
     .calendar-content {
-        z-index: 1001 !important;
+        z-index: 10 !important;
     }   
     .calendar-picker .calendar-for-picker {
-        z-index: 1001 !important;
-    }
-    .date-wrapper {
         z-index: 10 !important;
-    }
-    .select-wrapper {
-        z-index: 1001 !important;
     }
 </style>
 @endpush
